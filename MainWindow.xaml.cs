c631e17fa0379f2d8c7458a0b0f6c94e6352527c
Elettra Tegon <112578860+neTegon@users.noreply.github.com>
2023-12-18 19:02:08 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using elettra.tegon._4i.SlotMachineLibreria;
+namespace tegon.elettra._4i.SlotMachineWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SlotMachineLib s = new SlotMachineLib(10);
+        bool b1 = false;
+        bool b2 = false;
+        bool b3 = false;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnSlot_Click(object sender, RoutedEventArgs e)
+        {
+            while (s.Monete > 0)
+            {
+                s.Monete--;
+                for (int i = 0; i < 3; i++)
+                {
+                    if (b1 == true)
+                    {
+                        if (b2 == true)
+                        {
+                            s.carte("12");
+                        }
+                        else if (b3 == true)
+                        {
+                            s.carte("13");
+                        }
+                        else
+                        {
+                            s.carte("1");
+                        }
+
+                    }
+                    else if (b2 == true)
+                    {
+
+                        if (b3 == true)
+                        {
+                            s.carte("23");
+                        }
+                        else
+                        {
+                            s.carte("2");
+                        }
+                    }
+                    else if (b3 == true)
+                    {
+                        s.carte("3");
+                    }
+                    else
+                    {
+                        s.carte();
+                    }
+
+                    
+                    DesAlt.Text = Convert.ToString(s.Val1);
+                    CentAlt.Text = Convert.ToString(s.Val2);
+                     SinAlt.Text = Convert.ToString(s.Val3);
+                    DesBass.Text = Convert.ToString(s.Monete);
+                }
+                SinBass.Text = Convert.ToString(Convert.ToString(s.cambioMonete()));
+                
+            }
+        }
+        private void btnBlock1_CLick(object sender, RoutedEventArgs e)
+        {
+            if (b1==false)
+            {
+                
+                b1 = true;
+            }
+            else
+            {
+                b1 = false;
+            }
+        }
+        private void btnBlock2_CLick(object sender, RoutedEventArgs e)
+        {
+            if (b2 == false)
+            {
+
+                b2 = true;
+            }
+            else
+            {
+                b2 = false;
+            }
+        }
+        private void btnBlock3_CLick(object sender, RoutedEventArgs e)
+        {
+            if (b3 == false)
+            {
+
+                b3 = true;
+            }
+            else
+            {
+                b3 = false;
+            }
+        }
+    }
+    
+}
